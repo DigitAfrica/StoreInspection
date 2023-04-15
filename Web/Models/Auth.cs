ad76255db9bab653155b7fb69624891a4477d628
HGani <hgani786@gmail.com>
2023-04-15 14:57:29 +0200

    changed from BlazorWebAssembly to BlazorServer. login page WIP

---
+++ Web/Models/Auth.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models;
+
+public class Login
+{
+    [Required]
+    public string? Email { get; set; }
+
+    [Required]
+    public string? Password { get; set; }
+
+    public void LoginAsAdmin()
+    {
+        Email = "abc@xyz.com";
+        Password = "C@non123";
+    } //todo - remove when publishing
+}
---
+++ Web/Models/Shared.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models;
+
+public class ResponseMessage
+{
+    public bool Success { get; set; } = true;
+    public string? FullDescription { get; set; }
+    public string? ShortDescription { get; set; }
+    public string? Token { get; set; }
+    public object? Object { get; set; }
+}
