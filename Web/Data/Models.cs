da03dd133784a0adfd12033c6ff2c54a81e1d661
HGani <hgani786@gmail.com>
2023-04-16 15:33:42 +0200

    login session and navbar styling

---
+++ Web/Data/Models.cs
@@ -0,0 +1,39 @@
+﻿namespace Web.Data;
+
+public class User
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? NameSurname { get { return Name + " " + Surname; } }
+        public string? Token { get; set; }
+    }
+
+    public class Login
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        public void LoginAsAdmin()
+        {
+            Email = "abc@xyz.com";
+            Password = "Password";
+        } //todo - remove when publishing
+    }
+}
+
+public class Reply
+{
+    public bool Win { get; set; } = true;
+    public bool Fail { get; set; } = true;
+    public string? Msg { get; set; }
+    public string? Info { get; set; }
+    public object? Data { get; set; }
+}
---
+++ Web/Data/Session.cs
@@ -0,0 +1,47 @@
+﻿namespace Web.Data;
+
+public class Session
+{
+    public User.Info _user { get; set; } = new User.Info();
+
+    public void Login(User.Login loginDetails)
+    {
+        if (loginDetails == null)
+        {
+            _user = new User.Info();
+            return;
+        }
+
+        if (loginDetails.Email != "abc@xyz.com" || loginDetails.Password != "Password")
+        {
+            _user = new User.Info();
+            return;
+        }
+
+        _user = new User.Info()
+        {
+            Id = 1,
+            Email = loginDetails.Email,
+            Name = "FirstName",
+            Surname = "LastName",
+            Token = "JwtToken"
+        };
+    }
+
+    public bool IsLoggedIn()
+    {
+        if (string.IsNullOrEmpty(_user.Token)
+            || string.IsNullOrEmpty(_user.Email)
+            || _user.Id == 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void logOut()
+    {
+        _user = new User.Info();
+    }
+}
