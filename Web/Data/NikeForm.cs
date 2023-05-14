0a4852e44d92d11a3dc87ef7b654137acdf4bac9
HGani <hgani786@gmail.com>
2023-05-14 18:34:36 +0200

    make form and report more generic based on Questin sets

--- Web/Data/NikeForm.cs
+++ Web/Data/NikeForm.cs
@@ -1,41 +1,12 @@
-﻿namespace Web.Data;
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Xml.Linq;
 
-public class NikeForm
+namespace Web.Data;
+
+public class QInit
 {
-    public QLodgeSecurity QLodgeSecurity { get; set; } = new QLodgeSecurity();
-    public QPhysicalSecurity QPhysicalSecurity { get; set; } = new QPhysicalSecurity();
-
-    public int Total { get; set; } = 0;
-
-    public void Sum()
-    {
-        QLodgeSecurity.Sum();
-        QPhysicalSecurity.Sum();
-
-        Total = 0;
-        Total += QLodgeSecurity.ScoreTotal;
-        Total += QPhysicalSecurity.Total;
-    }
-}
-
-public class QLodgeSecurity
-{
-    public QLodgeSecurity()
-    {
-        Questions = new List<Question>();
-
-        for (int i = 0; i < listQuestions.Count(); i++)
-        {
-            Questions.Add(new Question(i + 1, listQuestions[i]));
-        }
-    }
-
-    public List<Question> Questions { get; set; }
-    public int ScoreTotal { get; set; } = 0;
-    public int ScoreMax { get; set; } = 0;
-    public int ScorePercent { get; set; } = 0;
-
-    public List<string> listQuestions = new List<string>()
+    public List<string> LodgeSecurity = new List<string>()
     {
         "Are all Security Officers wearing full uniform with their name badge?",
         //"Are Security Officers visible and vigilant whilst on duty at their post?",
@@ -51,6 +22,89 @@ public class QLodgeSecurity
         "Are staff purchases checked and recorded by the Security?",
         "Are staff items / apparel declared before entering the store - checked by Security?"
     };
+
+    public List<string> PhysicalSecurity = new List<string>()
+    {
+        //"Is the Alarm System working and used correctly daily?  Is the contact list up-to-date?",
+        //"Does the store have a panic button (connected to armed response, centre security)?",
+        //"Are the back door and the emergency doors alarmed when the store is open and trading?",
+        //"Is the perimeter security effective (check security gates, perimeter doors and fire escape)?",
+        //"Is the CCTV system working and recording correctly?",
+        //"Are all CCTV Cameras and devices working correctly?",
+        "Is the cash office closed and only accessible by managers?"
+    };
+
+    public List<string> SalesFloor = new List<string>()
+    {
+        //"Are EAS gates (store entrance) working and tested daily?",
+        //"Are EAS tags correctly applied to all relevant high-risk items?",
+        //"Are EAS tags correctly removed / deactivated at the tills?",
+        //"Are blind spots monitored / patrolled by Security & Staff?",
+        //"Are fitting room checks (pilfering) conducted on a regular basis during trading hours?",
+        //"Are pilfered items (tags / empty boxes / hangers) collected daily and recorded?",
+        //"Are tills correctly manned and is the Head Coach or Assistant Coach visible?",
+        "Is the sales floor neat and tidy (no clutter or boxes left in the aisle)?"
+    };
+
+    public List<string> Storeroom = new List<string>()
+    {
+        //"Are all fire exits and escape routes clear?",
+        //"Is all wastage checked before disposal?",
+        //"Is the receiving area neat and tidy (stock not left out in the open)?",
+        //"Are deliveries double checked against the invoice before being processed?",
+        //"Is the storeroom neat and tidy (no clutter or loose stock left in the storeroom)?",
+        //"Is there any evidence of pilfering in the storeroom?",
+        "Is the high-risk locker secured (unauthorised access restricted)?"
+    };
+}
+
+public class NikeForm
+{
+    public NikeForm()
+    {
+        ListQSet = new List<QSet>
+        {
+            new QSet("Lodge Security", QInit.LodgeSecurity),
+            new QSet("Physical Security", QInit.PhysicalSecurity),
+            new QSet("Sales Floor", QInit.SalesFloor),
+            new QSet("Storeroom", QInit.Storeroom)
+        };
+    }
+
+    public List<QSet> ListQSet { get; set; }
+    public QInit QInit { get; set; } = new QInit();
+    public int Total { get; set; } = 0;
+
+    public void Sum()
+    {
+        Total = 0;
+
+        foreach (var q in ListQSet)
+        {
+            q.Sum();
+            Total += q.ScoreTotal;
+        }
+    }
+}
+
+public class QSet
+{
+    public QSet(string title, List<string> listQuestions)
+    {
+        Title = title;
+        Questions = new List<Question>();
+
+        for (int i = 0; i < listQuestions.Count(); i++)
+        {
+            Questions.Add(new Question(i + 1, listQuestions[i]));
+        }
+    }
+
+    public string Title { get; set; }
+    public List<Question> Questions { get; set; }
+    public int ScoreTotal { get; set; } = 0;
+    public int ScoreMax { get; set; } = 0;
+    public int ScorePercent { get; set; } = 0;
 
     public void Sum()
     {
@@ -74,61 +128,8 @@ public class QLodgeSecurity
     {
         foreach (var q in Questions)
         {
-            if (q.Score == -5)
-            {
-                return false;
-            }
+            if (q.Score == -5) return false;
         }
-
-        return true;
-    }
-}
-
-public class QPhysicalSecurity
-{
-    public QPhysicalSecurity()
-    {
-        Questions = new List<Question>();
-
-        for (int i = 0; i < listQuestions.Count(); i++)
-        {
-            Questions.Add(new Question(i + 1, listQuestions[i]));
-        }
-    }
-
-    public List<Question> Questions { get; set; }
-    public int Total { get; set; } = 0;
-
-    public List<string> listQuestions = new List<string>()
-    {
-        //"Is the Alarm System working and used correctly daily?  Is the contact list up-to-date?",
-        //"Does the store have a panic button (connected to armed response, centre security)?",
-        //"Are the back door and the emergency doors alarmed when the store is open and trading?",
-        //"Is the perimeter security effective (check security gates, perimeter doors and fire escape)?",
-        //"Is the CCTV system working and recording correctly?",
-        //"Are all CCTV Cameras and devices working correctly?",
-        "Is the cash office closed and only accessible by managers?"
-    };
-
-    public void Sum()
-    {
-        Total = 0;
-        foreach (var q in Questions)
-        {
-            Total += q.Score;
-        }
-    }
-
-    public bool Validate()
-    {
-        foreach (var q in Questions)
-        {
-            if (q.Score == -5)
-            {
-                return false;
-            }
-        }
-
         return true;
     }
 }
@@ -145,25 +146,25 @@ public class Question
 
     public int MaxPossibleValue { get; set; } = 2;
 
-    public List<Response> Answers { get; set; }
+    public List<NameValue> Answers { get; set; }
 
     public Question(int num, string text)
     {
         Num = num;
         Text = text;
-        Answers = new List<Response>()
+        Answers = new List<NameValue>()
         {
-            new Response(-5, Text),
-            new Response(2, "Compliant"),
-            new Response(-1, "Non - Compliance"),
-            new Response(0, "Not Applicable"),
+            new NameValue(-5, Text),
+            new NameValue(2, "Compliant"),
+            new NameValue(-1, "Non - Compliance"),
+            new NameValue(0, "Not Applicable"),
         };
     }
 }
 
-public class Response
+public class NameValue
 {
-    public Response(int value, string name)
+    public NameValue(int value, string name)
     {
         Name = name;
         Value = value;
